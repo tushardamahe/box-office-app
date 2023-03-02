d994b39961e8308fae06e854b1487caa6c7df2cc
Tushar Damahe <qubetushar@gmail.com>
2023-03-02 14:55:15 +0530

    added navigation and shared layout for hoem and starred pages

---
+++ src/components/AppTitle.jsx
@@ -0,0 +1,13 @@
+export default function AppTitle(props) {
+  const {
+    title = "Box Office App",
+    subtitle = "Are You Looking For a Moive Or Actor?",
+  } = props;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
-
 const Home = () => {
   return <div>
-      <Link to="/starred">Go To Starred Page</Link>
+      Home
     </div>
 };
 
