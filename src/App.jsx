90fa9bcc74266b9932d1b382af8fe8f4b0f15e56
Tushar Damahe <qubetushar@gmail.com>
2023-03-02 14:06:22 +0530

    installed react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
