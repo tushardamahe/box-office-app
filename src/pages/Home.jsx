29f9749c48ff9c66742cce36a4a37ac0f13e4735
Tushar Damahe <qubetushar@gmail.com>
2023-03-03 00:15:47 +0530

    added textbox for searching, search for shows on button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div>
-      Home
+  const [searchStr, setSearchStr] = useState("");
+
+  const onSearchinputChange = (ev) => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async (ev) => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+
+    const body = await response.json();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchinputChange} />
+        <button type="submit">Search</button>
+      </form>
     </div>
+  );
 };
 
 export default Home;
