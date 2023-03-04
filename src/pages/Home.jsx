729c251392393a5aee016a6e9b45529d78014fd6
Tushar Damahe <qubetushar@gmail.com>
2023-03-04 21:43:27 +0530

    fetch and display data from tvmaze api, create reuseable function to fetch data (searchForShows)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { searchForShows } from "../api/tvmaze";
 
 const Home = () => {
   const [searchStr, setSearchStr] = useState("");
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
   const onSearchinputChange = (ev) => {
     setSearchStr(ev.target.value);
@@ -10,11 +13,27 @@ const Home = () => {
   const onSearch = async (ev) => {
     ev.preventDefault();
 
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    );
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
 
-    const body = await response.json();
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occured: {apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+
+    return null;
   };
 
   return (
@@ -23,6 +42,10 @@ const Home = () => {
         <input type="text" value={searchStr} onChange={onSearchinputChange} />
         <button type="submit">Search</button>
       </form>
+
+      <div>
+        <div>{renderApiData()}</div>
+      </div>
     </div>
   );
 };
